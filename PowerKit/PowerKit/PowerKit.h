1f3c9e98047987c05a6c842ed84b1ecaec638846
XU SHIYAN <xushiyan.1990@gmail.com>
2015-12-06 22:04:05 +0800

    newly named project

---
+++ PowerKit/PowerKit/PowerKit.h
@@ -0,0 +1,19 @@
+//
+//  PowerKit.h
+//  PowerKit
+//
+//  Created by XU SHIYAN on 6/12/15.
+//  Copyright © 2015 PowerStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PowerKit.
+FOUNDATION_EXPORT double PowerKitVersionNumber;
+
+//! Project version string for PowerKit.
+FOUNDATION_EXPORT const unsigned char PowerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PowerKit/PublicHeader.h>
+
+
